276a61890e1110883e119113a91e21037bcef30d
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-18 19:04:02 -0300

    UPDATE<errors>: handler

--- app/backend/src/errors/index.ts
+++ app/backend/src/errors/index.ts
@@ -1,7 +1,11 @@
 import InvalidToken from './InvalidToken';
 import TokenNotFound from './TokenNotFound';
+import NotFound from './NotFound';
+import BadRequest from './BadRequest';
 
 export {
   InvalidToken,
   TokenNotFound,
+  NotFound,
+  BadRequest,
 };
