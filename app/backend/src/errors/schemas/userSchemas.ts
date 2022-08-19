f94dbd41c5598b829523bf85d9294a62f5c1295c
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-19 19:01:17 -0300

    FEAT<schemas>: User validations

--- app/backend/src/errors/schemas/userSchemas.ts
+++ app/backend/src/errors/schemas/userSchemas.ts
@@ -1,22 +1,27 @@
 import * as Joi from 'joi';
 
 const userSchema: Record<string, Joi.ObjectSchema> = {
+  loginFields: Joi.object({
+    email: Joi.string().required().messages({
+      'any.required': 'All fields must be filled',
+    }),
+    password: Joi.string().required().messages({
+      'any.required': 'All fields must be filled',
+    }),
+  }).required(),
   login: Joi.object({
     email: Joi.string()
       .email()
       .min(1)
-      .required()
       .messages({
-        'any.required': 'email obrigatório',
+        'string.email': 'Email must be a valid email',
       })
       .required(),
     password: Joi.string()
       .min(6)
-      .required()
       .messages({
-        'any.required': 'email obrigatório',
-      })
-      .required(),
+        'string.min': 'Password must have at least 6 characters',
+      }),
   }).required(),
 };
 
