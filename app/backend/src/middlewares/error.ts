d85080e2b78fb186806abedfdd15e72ea86cf4b9
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-19 18:23:56 -0300

    FEAT<errors>: new interfaces and expected errors

--- app/backend/src/middlewares/error.ts
+++ app/backend/src/middlewares/error.ts
@@ -1,11 +1,30 @@
 import { ErrorRequestHandler } from 'express';
 
+interface ExpectedErrors {
+  name: string;
+  code: number;
+}
+
+const expectedErrors: ExpectedErrors[] = [
+  { name: 'validationError', code: 400 },
+  { name: 'invalidLogin', code: 401 },
+  { name: 'notFound', code: 404 },
+  { name: 'tokenNotFound', code: 400 },
+  { name: 'invalidToken', code: 401 },
+];
+
 const errorMiddleware: ErrorRequestHandler = (err: Error, _req, res, _next) => {
-  if (err.name === 'ValidationError') {
-    return res.status(400).json({ message: err });
+  const errorName: string = err.name.toLowerCase();
+  const expectedError: ExpectedErrors | undefined = expectedErrors.find(
+    (expectedErr) => expectedErr.name.toLowerCase() === errorName,
+  );
+
+  if (!expectedError) {
+    return res.status(500).json({ message: err.message });
   }
 
-  return res.status(500).json({ message: err });
+  const { code } = expectedError;
+  return res.status(code).json({ message: err.message });
 };
 
 export default errorMiddleware;
