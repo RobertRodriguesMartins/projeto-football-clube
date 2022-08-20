3e9aa711d70e243e002aec0c6e23520c76c06261
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-20 13:31:24 -0300

    FEAT<tests>: User mock for test purposes

--- app/backend/src/tests/mocks/User.ts
+++ app/backend/src/tests/mocks/User.ts
@@ -1,9 +1,10 @@
-import { IUser } from "../../database/models/interfaces";
-import { hashSync } from "bcryptjs";
+import { IUser } from '../../database/models/interfaces';
+import { hashSync } from 'bcryptjs';
+
 export const userMock: IUser = {
   id: 1,
-  username: "teste",
-  password: hashSync("teste123"),
-  email: "testemail@gmail.com",
-  role: "cool"
-}
+  username: 'teste',
+  password: hashSync('teste123'),
+  email: 'testemail@gmail.com',
+  role: 'cool',
+};
