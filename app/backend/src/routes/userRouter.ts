c5664cb4c8f06951239ad7f98a937b2624a40fcc
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-20 13:28:34 -0300

    FEAT<router>: New route for User

--- app/backend/src/routes/userRouter.ts
+++ app/backend/src/routes/userRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userController from '../controller/userController';
+import { Token } from '../middlewares';
 
 const userRouter = Router();
 
-userRouter.post('/', userController.login);
+userRouter.post('/', userController.login)
+  .get('/validate', Token.validateToken, userController.validate);
 
 export default userRouter;
