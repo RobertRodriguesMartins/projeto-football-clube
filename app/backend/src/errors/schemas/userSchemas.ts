d919d680740303a8f6b6429f5aa32054a3d8a1b5
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-20 13:27:58 -0300

    FIX<schemas>: User case when request data is empty

--- app/backend/src/errors/schemas/userSchemas.ts
+++ app/backend/src/errors/schemas/userSchemas.ts
@@ -1,27 +1,30 @@
 import * as Joi from 'joi';
 
+const requiredFields = 'All fields must be filled';
+
 const userSchema: Record<string, Joi.ObjectSchema> = {
   loginFields: Joi.object({
-    email: Joi.string().required().messages({
-      'any.required': 'All fields must be filled',
-    }),
-    password: Joi.string().required().messages({
-      'any.required': 'All fields must be filled',
-    }),
+    email: Joi.string().empty('').required().messages({
+      'any.required': requiredFields,
+      'string.empty': requiredFields,
+    })
+      .required(),
+    password: Joi.string().empty('').required().messages({
+      'any.required': requiredFields,
+      'string.empty': requiredFields,
+    })
+      .required(),
   }).required(),
   login: Joi.object({
     email: Joi.string()
       .email()
-      .min(1)
       .messages({
         'string.email': 'Email must be a valid email',
       })
       .required(),
-    password: Joi.string()
-      .min(6)
-      .messages({
-        'string.min': 'Password must have at least 6 characters',
-      }),
+    password: Joi.string().min(6).messages({
+      'string.min': 'Password must have at least 6 characters',
+    }),
   }).required(),
 };
 
