7afb133f842c3cd5eb0891f5efc8c476b95538fe
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-23 12:52:15 -0300

    UPDATE<schemas>: handler

--- app/backend/src/errors/schemas/index.ts
+++ app/backend/src/errors/schemas/index.ts
@@ -1,8 +1,10 @@
 import * as Joi from 'joi';
 import userSchema from './userSchemas';
+import teamSchema from './teamSchemas';
 
 const schemas: Record<string, Joi.ObjectSchema> = {
   ...userSchema,
+  ...teamSchema,
 };
 
 export default schemas;
