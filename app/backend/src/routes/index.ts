f0b59d65c759dd12cb613fa55e524b8b3ddccd8b
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-24 13:20:24 -0300

    UPDATE<routes>: handler

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import teamRouter from './teamRouter';
 import userRouter from './userRouter';
+import matchRouter from './matchRouter';
 
-export { teamRouter, userRouter };
+export { teamRouter, userRouter, matchRouter };
