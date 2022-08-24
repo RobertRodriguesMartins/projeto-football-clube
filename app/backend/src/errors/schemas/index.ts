84e34e16b301fd94098d333140f599168554b4bb
Robert Rodrigues <robert.rm131@gmail.com>
2022-08-24 13:21:24 -0300

    UPDATE<schemas>: handler

--- app/backend/src/errors/schemas/index.ts
+++ app/backend/src/errors/schemas/index.ts
@@ -1,10 +1,12 @@
 import * as Joi from 'joi';
 import userSchema from './userSchemas';
 import teamSchema from './teamSchemas';
+import matchSchema from './matchSchemas';
 
 const schemas: Record<string, Joi.ObjectSchema> = {
   ...userSchema,
   ...teamSchema,
+  ...matchSchema,
 };
 
 export default schemas;
